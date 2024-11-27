198db6e3f9e305ad88fe4c6ded5a87344381e1b8
Jose Carlos Gutierrez <joseco48@gmail.com>
2024-11-27 14:07:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
-var cors = require('cors');
+const path = require('path');
 
 const app = express();
 const PORT = 80;
 
 // Middleware
-app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Simula una base de datos con un arreglo
 const data = {
